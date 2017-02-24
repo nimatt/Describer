4d2ba29b9070033f94b8a9813d89dd27dc59a48c
Mattias Nilsson <mattias.nilsson@sweco.se>
2017-02-24 19:09:50 +0100

    Initial commit

---
+++ Describer.Test/Tests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Describer.Test
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void First()
+        {
+            Assert.IsNotNull(new TestClass().ToString());
+        }
+
+        private class TestClass
+        {
+            public string FirstProp { get; set; }
+
+            public TestClass()
+            {
+                FirstProp = "first";
+            }
+
+            public override string ToString()
+            {
+                return DescriptionBuilder.Describe(this,
+                        o => o.FirstProp
+                    );
+            }
+        }
+    }
+}
