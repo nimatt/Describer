397c28fe36112d623264476c28d95243735f672e
Mattias Nilsson <nimatt86@gmail.com>
2017-02-25 00:26:46 +0100

    Fixed simple wrapping functions

--- Describer/Logic.fs
+++ Describer/Logic.fs
@@ -4,19 +4,24 @@ open System
 open System.Linq.Expressions
 
 type private Builder<'b> (exps: seq<Expression<Func<'b,string>>>) =
-    let isLastExpr (exp: Expression) = exp.NodeType = ExpressionType.Parameter
 
-    let rec getPropertyName (exp: Expression) = 
+    let rec getPropertyName (exp: Expression) = getPropertyNameInternal None exp
+    and getPropertyNameInternal lastFound (exp: Expression) = 
         match exp with
-            | :? MemberExpression as memExp -> if isLastExpr (memExp.Expression) then memExp.Member.Name
-                                               else getPropertyName (memExp.Expression)
-            | :? MethodCallExpression as methExp -> if isLastExpr (methExp.Object) then methExp.Method.Name + "()"
-                                                    else getPropertyName (methExp.Object)
-            | :? ParameterExpression as paramExp -> paramExp.Name
-            | _ -> exp.GetType().Name
+            | :? MemberExpression as memExp -> getPropertyNameInternal (Some memExp.Member.Name) memExp.Expression
+            | :? MethodCallExpression as methExp -> getNameFromMethodCall methExp
+            | :? ParameterExpression -> lastFound
+            | :? ConstantExpression -> None
+            | _ -> Some (exp.GetType().Name)
+
+    and getNameFromMethodCall (methExp: MethodCallExpression) = 
+        let directPath = getPropertyNameInternal (Some (methExp.Method.Name + "()")) methExp.Object
+        directPath :: (methExp.Arguments |> Seq.map (fun arg -> getPropertyName arg) |> List.ofSeq)
+             |> Seq.where (fun res -> res.IsSome) 
+             |> Seq.head
 
     let compiled =
-        exps |> Seq.map (fun e -> (getPropertyName (e.Body), e.Compile()))
+        exps |> Seq.map (fun e -> ((getPropertyName (e.Body)).Value, e.Compile()))
 
     member this.Evaluate (o: 'b) = 
         compiled |> Seq.map (fun ce -> fst ce + ": " + (snd ce).Invoke(o)) |> (fun rw -> String.Join(", ", rw))
